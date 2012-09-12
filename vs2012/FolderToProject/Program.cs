6d7e4da0ff2779e89326588d5404984454c40cec
sparkie <dark_sparkie@hotmail.com>
2012-09-12 15:54:30 +0200

    Filters are working as well now.

--- vs2012/FolderToProject/Program.cs
+++ vs2012/FolderToProject/Program.cs
@@ -12,6 +12,8 @@ namespace FolderToProject
     {
         string projectFile;
         string filterFile;
+        string unfilteredFile = "";
+        int filterPart = 0;
 
         Program(string targetDir, string projectFileName, string filterFileName)
         {
@@ -21,7 +23,13 @@ namespace FolderToProject
             AddBeginProjectFile();
             AddBeginFilterFile();
 
+            filterFile += "  <ItemGroup>\r\n";
+            IterateDirFilters(targetDir, "");
+            filterFile += "  </ItemGroup>\r\n";
+            filterFile += "  <ItemGroup>\r\n";
             IterateDir(targetDir, "");
+            filterFile += unfilteredFile;
+            filterFile += "  </ItemGroup>\r\n";
 
             AddEndProjectFile();
             AddEndFilterFile();
@@ -32,6 +40,24 @@ namespace FolderToProject
             StreamWriter sw2 = new StreamWriter(filterFileName, false);
             sw2.Write(filterFile);
             sw2.Close();
+        }
+
+        void IterateDirFilters(string targetDir, string filterName)
+        {
+            DirectoryInfo[] directoryEntries = new DirectoryInfo(targetDir).GetDirectories();
+            
+            foreach (DirectoryInfo s in directoryEntries)
+            {
+                filterFile += "    <Filter Include=\"" + s.Name + "\">\r\n      <Extensions>cpp;c;cxx;rc;def;r;odl;idl;hpj;bat;h;hpp;hxx;hm;inl</Extensions>\r\n    </Filter>\r\n";
+            }
+      
+            foreach (DirectoryInfo s in directoryEntries)
+            {
+                if (filterName != "")
+                    IterateDirFilters(targetDir + "\\" + s.Name, filterName + "\\" + s.Name);
+                else
+                    IterateDirFilters(targetDir + "\\" + s.Name, s.Name);
+            }
         }
 
         void IterateDir(string targetDir, string filterName)
@@ -49,33 +75,35 @@ namespace FolderToProject
             if (fileEntries.Count > 0)
             {
                 // Project file part
-                projectFile += "<ItemGroup>\r\n";
+                projectFile += "  <ItemGroup>\r\n";
                 foreach (string s in fileEntries)
                 {
-                    projectFile += " <ClInclude Include=\"" + s + "\" />\r\n";
+                    projectFile += "   <ClInclude Include=\"" + s + "\" />\r\n";
                 }
-                projectFile += "</ItemGroup>\r\n";
+                projectFile += "  </ItemGroup>\r\n";
 
                 // Filter part
-                filterFile += "<ItemGroup>\r\n";
-
                 foreach (string s in fileEntries)
                 {
-                    filterFile += "<ClCompile Include=\"" + s + "\">\r\n";
                     if (filterName != "")
-                        filterFile += "<Filter>" + filterName + "</Filter>\r\n";
-                    filterFile += "</ClCompile>\r\n";
+                    {
+                        filterFile += "    <ClInclude Include=\"" + s + "\">\r\n";
+                        filterFile += "      <Filter>" + filterName + "</Filter>\r\n";
+                        filterFile += "    </ClInclude>\r\n";
+                    }
+                    else
+                    {
+                        unfilteredFile += "    <ClInclude Include=\"" + s + "\" />\r\n";
+                    }
                 }
-
-                filterFile += "</ItemGroup>\r\n";
 
                 DirectoryInfo[] directoryEntries = new DirectoryInfo(targetDir).GetDirectories();
                 foreach (DirectoryInfo s in directoryEntries)
                 {
                     if (filterName != "")
-                        IterateDir(s.FullName, filterName + "\\" + s.Name);
+                        IterateDir(targetDir + "\\" + s.Name, filterName + "\\" + s.Name);
                     else
-                        IterateDir(s.FullName, s.Name);
+                        IterateDir(targetDir + "\\" + s.Name, s.Name);
                 }
             }
         }
@@ -90,12 +118,12 @@ namespace FolderToProject
             StreamReader reader = new StreamReader("projectFileBegin.txt");
             projectFile = reader.ReadToEnd();
             reader.Close();
+            projectFile += "\r\n";
         }
 
         void AddEndProjectFile()
         {
             StreamReader reader = new StreamReader("projectFileEnd.txt");
-            projectFile += "\r\n";
             projectFile += reader.ReadToEnd();
             reader.Close();
         }
@@ -105,12 +133,12 @@ namespace FolderToProject
             StreamReader reader = new StreamReader("filterFileBegin.txt");
             filterFile = reader.ReadToEnd();
             reader.Close();
+            filterFile += "\r\n";
         }
 
         void AddEndFilterFile()
         {
             StreamReader reader = new StreamReader("filterFileEnd.txt");
-            filterFile += "\r\n";
             filterFile += reader.ReadToEnd();
             reader.Close();
         }
