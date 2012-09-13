e9685875163fb486eaec4114e1b15a9b8ce80a2c
Gerjo <gerjoo@gmail.com>
2012-09-13 15:46:13 +0200

    Netbeans will now also halt on warnings.

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -64,7 +64,7 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/pccs: ${OBJECTFILES}
 ${OBJECTDIR}/src/main.o: src/main.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/main.o src/main.cpp
+	$(COMPILE.cc) -g -Werror -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/main.o src/main.cpp
 
 # Subprojects
 .build-subprojects:
