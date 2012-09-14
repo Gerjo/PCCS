627f7a436e7f2b1ef3c9838cd84f0865d6824374
Gerjo <gerjoo@gmail.com>
2012-09-14 16:23:35 +0200

    added the gui to the correct project, this time.

--- Downloader/src/com/pccs/main/Main.java
+++ Downloader/src/com/pccs/main/Main.java
@@ -1,8 +1,25 @@
 package com.pccs.main;
 
+import com.pccs.gui.Panel;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Downloader works!");
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        
+        JFrame window = new JFrame("Guerrilla Tactics downloader.");
+        window.setSize(400, 350);
+        window.setResizable(false);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+        window.getContentPane().add(new Panel());
+        
     }
 }
