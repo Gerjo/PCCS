2be370eab7613724657de4b7d7b1ccd3bd043bbb
Gerjo <gerjoo@gmail.com>
2012-09-14 16:21:29 +0200

    added gui

--- Master Server/src/com/pccs/main/Main.java
+++ Master Server/src/com/pccs/main/Main.java
@@ -1,8 +1,29 @@
 package com.pccs.main;
 
+import com.pccs.gui.Panel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Master server works.");
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        
+        JFrame window = new JFrame("Guerrilla Tactics downloader.");
+        window.setSize(400, 350);
+        window.setResizable(false);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+        window.getContentPane().add(new Panel());
+        
     }
 }
