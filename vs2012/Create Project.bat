6b51c3184e48c8a20f4a8f1fbebf235d99290f47
sparkie <dark_sparkie@hotmail.com>
2012-09-15 17:47:59 +0200

    Created "game" folder for a cleaner layout of our root folder.

--- vs2012/Create Project.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-cd vs2012\FolderToProject
-FolderToProject.exe "..\..\phantom\src" "..\CpPhantom\CpPhantom.vcxproj"
-FolderToProject.exe "..\..\src" "..\GuerrillaTactics\GuerrillaTactics.vcxproj"
