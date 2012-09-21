213b2c715a39182b00ab3fb5ef84b69e8c8d8633
sparkie <dark_sparkie@hotmail.com>
2012-09-21 21:56:20 +0200

    Removed sockets. New phantom hash. Changed project settings.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -4,50 +4,18 @@
 
 using namespace std;
 
-#include "sockets/Sockets.h"
+#include <phantom.h>
+#include <graphics/GLGraphics.h>
+#include <input/GLUTInputState.h>
 
 int main(int argc, char *argv[]) {
-    
-    try {
-        cout << "Connecting to master server..." << endl;
-        Socket socket("cis.gerardmeier.com", "8078");
-        cout << "Connected. Checking for updates" << endl;
-        
-        OutputStream& out = socket.getOutputStream();
-        InputStream& in = socket.getInputStream();
-        
-        out.write("{\"builds\":true}\n");
+	phantom::PhantomGame	*game		=	new phantom::PhantomGame("settings.yaml");
+	phantom::Graphics		*graphics	=	new phantom::GLGraphics();
+	phantom::InputState		*input		=	new phantom::GLUTInputState();
 
-        bool isRunning = true;
-        char current = 0;
-        char prev    = 0;
-        stringstream buff;
-        do {
-            
-            
-            int available = in.available();
-            if(available > 0) {
-                prev    = current;
-                current = in.read();
-                buff << current;
-            }
-            
-            // Account for escaped newlines.
-            isRunning = !(current == '\n' && prev != '\\');
-            
-        } while(isRunning);
-        
-        cout << " Reply from the server: " << endl;
-        cout << buff.str() << endl;
-        
-    } catch(const SocketException& ex) {
-        cout << "Unable to connect to master server." << endl;
-    }
-    
-    
-    Game game();
-    
-    cout << " Press any key to get free cookies." << endl;
-    
-    return cin.get();
+	game->addComponent(graphics);
+	game->addComponent(input);
+
+	game->start(argc, argv);
+	return 0;
 }
