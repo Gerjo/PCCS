98eaa1f5f612034fa2ae7fd7ae0928fdbfc04971
Gerjo <gerjoo@gmail.com>
2012-09-24 15:00:51 +0200

    Fixed it for gcc.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -8,11 +8,17 @@ using namespace std;
 #include <graphics/GLUTRenderer.h>
 #include <input/GLUTInputState.h>
 #include <graphics/GLUTDriver.h>
+#include <graphics/BaseDriver.h>
+
 
 int main(int argc, char *argv[]) {
 	phantom::PhantomGame	*game		=	new phantom::PhantomGame("settings.yaml");
-    game->start(argc, argv, &phantom::GLUTDriver());
+
+    phantom::BaseDriver* base = new phantom::GLUTDriver();
+
+    game->start(argc, argv, base);
 
 	delete game;
+    delete base;
 	return 0;
 }
