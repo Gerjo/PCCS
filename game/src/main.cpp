e338e8d70ffab347ec261a33554d3381595e8cb3
Descalon <info@nicoglas.com>
2012-09-24 16:45:06 +0200

    Testing my rect

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -10,13 +10,20 @@ using namespace std;
 #include <graphics/GLUTDriver.h>
 
 int main(int argc, char *argv[]) {
-	phantom::PhantomGame	*game		=	new phantom::PhantomGame("settings.yaml");
-	//game->setRenderer(renderer);
-	//game->addComponent(input);
-
+    phantom::PhantomGame	*game		=	new phantom::PhantomGame("settings.yaml");
+    phantom::GameState* state = new phantom::GameState();
+    game->pushGameState(state);
+    phantom::Composite* meh = new phantom::Composite();
+    meh->getGraphics()->beginPath();
+    meh->getGraphics()->setFillStyle(phantom::Color(255,0,0,255));
+    meh->getGraphics()->setLineStyle(phantom::Color(0,255,0,255));
+    meh->getGraphics()->rect(0.10,0.10,0.25,0.25);
+    meh->getGraphics()->beginPath();
+    state->addComponent(meh);
     game->start(argc, argv, &phantom::GLUTDriver());
 
-	delete game;
+    delete meh;
+    delete game;
 
-	return 0;
+    return 0;
 }
