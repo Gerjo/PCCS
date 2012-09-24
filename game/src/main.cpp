a0d1fd5f6a29e80bab80c7ba2b6d9450e897c52f
Descalon <info@nicoglas.com>
2012-09-24 13:43:11 +0200

    Added the GLUTDriver to main

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,19 +7,15 @@ using namespace std;
 #include <phantom.h>
 #include <graphics/GLUTRenderer.h>
 #include <input/GLUTInputState.h>
+#include <graphics/GLUTDriver.h>
 
 int main(int argc, char *argv[]) {
 	phantom::PhantomGame	*game		=	new phantom::PhantomGame("settings.yaml");
-	phantom::Renderer		*renderer	=	new phantom::GLUTRenderer(800, 600);
-	phantom::InputState		*input		=	new phantom::GLUTInputState();
+	//game->setRenderer(renderer);
+	//game->addComponent(input);
 
-	game->setRenderer(renderer);
-	game->addComponent(input);
+    game->start(argc, argv, &phantom::GLUTDriver());
 
-	game->start(argc, argv);
-
-	delete input;
-	delete renderer;
 	delete game;
 
 	return 0;
