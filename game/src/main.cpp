f4b64acdf65ad6457dd7d9f153996cf1bf64a679
sparkie <dark_sparkie@hotmail.com>
2012-09-26 15:03:47 +0200

    hash

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -19,16 +19,16 @@ int main(int argc, char *argv[]) {
     meh->getGraphics()->setFillStyle(phantom::Color(127,0,0,127));
     meh->getGraphics()->setLineStyle(phantom::Color(0,127,0,127));
     
-    meh->getGraphics()->arc(500,250,75,0,(2*M_PI));
+    meh->getGraphics()->arc(500,250,100,0,(2*M_PI));
 
-	for(unsigned int i = 0; i < 128; ++i)
+	/*for(unsigned int i = 0; i < 128; ++i)
 	{
 		for(unsigned int j = 0; j < 72; ++j)
 		{
 			meh->getGraphics()->line(i*50, 0, i*50, j*50);
 			meh->getGraphics()->line(0, j*50, i*50, j*50);
 		}
-	}
+	}*/
 
     meh->getGraphics()->beginPath();
     state->addComponent(meh);
