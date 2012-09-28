f82bc6d4b817ccefd7a8e496f4004b5cef2a487a
sparkie <dark_sparkie@hotmail.com>
2012-09-28 21:18:05 +0200

    Added welcome message to the console.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -13,7 +13,11 @@ using namespace std;
 #include "Game.h"
 
 int main(int argc, char *argv[]) {
-
+    cout <<   "   _____ _____ _____ _____  " << endl <<
+              "  |  _  |     |     |   __| " << endl <<
+              "  |   __|  ===|  ===|__   | " << endl <<
+              "  |__|  |_____|_____|_____| " << endl <<
+              "                            " << endl;
     Game game("   _____ _____ _____ _____  "
               "  |  _  |     |     |   __| "
               "  |   __|  ===|  ===|__   | "
