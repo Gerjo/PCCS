baad75250dc9a537973060be532bcdf1f83875cc
Gerjo <gerjoo@gmail.com>
2012-10-01 13:51:12 +0200

    Moved images to their own folder.

--- game/src/gameobjects/Water.cpp
+++ game/src/gameobjects/Water.cpp
@@ -13,7 +13,7 @@ void Water::draw() {
     phantom::Graphics *gfx = getGraphics();
     gfx->beginPath();
     gfx->setFillStyle(phantom::Color(0, 0, 127, 127));
-    gfx->image("Water 80x80.png", 0, 0, 80, 80);
+    gfx->image("images/Water 80x80.png", 0, 0, 80, 80);
     gfx->beginPath();
 }
 
