62adcdf4a6b62eda60337b2b813b289c7216e3a8
Descalon <info@nicoglas.com>
2012-10-01 13:32:39 +0200

    Removed tol

--- game/src/Game.cpp
+++ game/src/Game.cpp
@@ -13,10 +13,6 @@ Game::Game(const char* configfile) : PhantomGame(configfile) {
 
     Composite* composite = new Tank();
     _gameState.addComponent(composite);
-    
-    phantom::TiledObjectLayer* tol = new phantom::TiledObjectLayer();
-    tol->createtiles(10,5,5);
-    
     Composite* tree = new Tree();
     _gameState.addComponent(tree);
 
