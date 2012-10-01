2753d78ef91bd08535747e4adaf2b3e0baa9c935
Gerjo <gerjoo@gmail.com>
2012-10-01 15:30:19 +0200

    Fancy new syntax.

--- game/src/gameobjects/Ground.cpp
+++ game/src/gameobjects/Ground.cpp
@@ -5,12 +5,11 @@ Ground::Ground() {
 }
 
 void Ground::draw(void) {
-    Graphics& gfx = getGraphics();
-    gfx.setFillStyle(phantom::Color(0, 0, 0, 127));
-    gfx.rect(0, 0, 12, 12);
-    gfx.beginPath();
-
-    gfx.setFillStyle(phantom::Color(50, 10, 0, 127));
-    gfx.rect(1, 1, 11, 11);
-    gfx.beginPath();
+    getGraphics()
+    .setFillStyle(Color(0, 0, 0))
+    .rect(0, 0, 12, 12)
+    .beginPath()
+    .setFillStyle(Color(50, 10, 0))
+    .rect(1, 1, 11, 11)
+    .beginPath();
 }
