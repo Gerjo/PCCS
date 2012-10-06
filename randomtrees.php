38b2a2617693976201d46514d7916806bc9fb7d8
Gerjo <gerjoo@gmail.com>
2012-10-06 22:37:28 +0200

    A simple php script to generate 100 random trees.

---
+++ randomtrees.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = "";
+
+for($i = 0; $i < 100; ++$i) {
+	$x = mt_rand(0, 2000);
+	$y = mt_rand(0, 2000);
+
+	$data .= '{ "type": "tree", "x": ' . $y . ', "y": ' . $x . ' },' . PHP_EOL;	
+}
+
+
+print trim(trim($data), ',');
