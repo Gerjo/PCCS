9ec7c0f124a260901f2e437bd96165bfaddf5683
Descalon <info@nicoglas.com>
2012-10-08 13:39:29 +0200

    Made changes to the php and json files. Added the enemy

--- randomtrees.php
+++ randomtrees.php
@@ -2,12 +2,19 @@
 
 $data = "";
 
+for($i = 0; $i < 20; ++$i) {
+	$x = mt_rand(0, 2000);
+	$y = mt_rand(0, 2000);
+
+	$data .= '{ "type": "crate", "x": ' . $y . ', "y": ' . $x . ' },' . PHP_EOL;	
+}
 for($i = 0; $i < 100; ++$i) {
 	$x = mt_rand(0, 2000);
 	$y = mt_rand(0, 2000);
 
 	$data .= '{ "type": "tree", "x": ' . $y . ', "y": ' . $x . ' },' . PHP_EOL;	
 }
+
 
 
 print trim(trim($data), ',');
