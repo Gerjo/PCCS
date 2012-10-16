24a90b60aa38d71d9039ceb4dc808b868b008dd9
sparkie <dark_sparkie@hotmail.com>
2012-10-16 17:22:35 +0200

    Submodule libyaxl updated.
    Submodule libyaxl:
        > faa3690 - Added auto generated project file.

--- Create Project.bat
+++ Create Project.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 cd foldertoproject
-FolderToProject.exe "..\phantom\src" "..\phantom\phantom.vcxproj"
+FolderToProject.exe "..\dedicatedserver\src" "..\dedicatedserver\dedicatedserver.vcxproj"
 FolderToProject.exe "..\game\src" "..\game\guerrillatactics.vcxproj"
-FolderToProject.exe "..\dedicatedserver\src" "..\libyaxl\libyaxl" "..\dedicatedserver\dedicatedserver.vcxproj"
+FolderToProject.exe "..\libyaxl\libyaxl" ..\libyaxl\yaxl.vcxproj"
+FolderToProject.exe "..\phantom\src" "..\phantom\phantom.vcxproj"
 FolderToProject.exe "..\sharedlib\src" "..\sharedlib\sharedlib.vcxproj"
 cd ..
