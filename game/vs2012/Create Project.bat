3fdb3451ca3e68ac9bdb271d8c5bf10df802ca5c
sparkie <dark_sparkie@hotmail.com>
2012-10-16 13:27:59 +0200

    Added extra projects.

--- game/vs2012/Create Project.bat
+++ game/vs2012/Create Project.bat
@@ -4,3 +4,5 @@ cd vs2012
 cd FolderToProject
 FolderToProject.exe "..\..\..\phantom\src" "..\CpPhantom\CpPhantom.vcxproj"
 FolderToProject.exe "..\..\src" "..\GuerrillaTactics\GuerrillaTactics.vcxproj"
+FolderToProject.exe "..\..\..\dedicatedserver\src" "..\Dedicated Server\Dedicated Server.vcxproj"
+FolderToProject.exe "..\..\..\sharedlib\src" "..\Shared\Shared.vcxproj"
