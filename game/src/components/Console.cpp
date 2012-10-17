65bd619d613de004df1f87335ff6d9a404479814
sparkie <dark_sparkie@hotmail.com>
2012-10-17 12:29:31 +0200

    no longer has reference to freeglut

--- game/src/components/Console.cpp
+++ game/src/components/Console.cpp
@@ -1,7 +1,5 @@
 #include "Console.h"
 
-#include <gl/freeglut.h>
-
 Console* Console::INSTANCE = 0;
 
 void Console::log(string log) {
@@ -47,7 +45,7 @@ void Console::renderText(int offset, Color color) {
     float lineOffset = lineheight;
 
     for(string& log : _logs) {
-        g.text(static_cast<float>(offset), lineOffset + static_cast<float>(offset), GLUT_BITMAP_HELVETICA_10, &log);
+        g.text(static_cast<float>(offset), lineOffset + static_cast<float>(offset), ((void *)0x0006), &log);
         lineOffset += lineheight;
     }
 
