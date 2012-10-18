1a90e0cdb6845bf162aeeef476c59da573467a6d
sparkie <dark_sparkie@hotmail.com>
2012-10-18 19:14:44 +0200

    Nice try KDiff

--- game/src/components/Console.cpp
+++ game/src/components/Console.cpp
@@ -46,7 +46,6 @@ void Console::renderText(int offset, Color color) {
 
     for(string& log : _logs) {
         g.text(static_cast<float>(offset), lineOffset + static_cast<float>(offset), 16, "fonts/arial.ttf", &log);
-        g.text(static_cast<float>(offset), lineOffset + static_cast<float>(offset), 16, &meh, &log);
         lineOffset += lineheight;
     }
 
