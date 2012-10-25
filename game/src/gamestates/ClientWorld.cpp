69f91ad5391ba1e4f412b8b0d024b88af28e95e5
sparkie <dark_sparkie@hotmail.com>
2012-10-25 20:39:30 +0200

    Project file changes. Debugging symbols don't overwrite each other now. Uncommented the commented part.

--- game/src/gamestates/ClientWorld.cpp
+++ game/src/gamestates/ClientWorld.cpp
@@ -40,23 +40,23 @@ void ClientWorld::init(void) {
 }
 
 void ClientWorld::load(string json) {
-    //cout << json << endl;
-    //Data data = Data::fromJson(json);
+    cout << json << endl;
+    Data data = Data::fromJson(json);
 
-    //for(Data::KeyValue pair : data("static")) {
-    //    Data& description = pair.second;
+    for(Data::KeyValue pair : data("static")) {
+        Data& description = pair.second;
 
-    //    _commands.add([this, description] () mutable -> void {
-    //        GameObject* gameObject = HeavyFactory::create(description("type"));
-    //        gameObject->fromData(description);
+        _commands.add([this, description] () mutable -> void {
+            GameObject* gameObject = HeavyFactory::create(description("type"));
+            gameObject->fromData(description);
 
-    //        gameobjects->addComponent(gameObject);
-    //    });
-    //}
+            gameobjects->addComponent(gameObject);
+        });
+    }
 
-    //_commands.add([this] (void) {
-    //    getGame<Game*>()->startPlaying();
-    //});
+    _commands.add([this] (void) {
+        getGame<Game*>()->startPlaying();
+    });
 }
 
 void ClientWorld::update(const float& elapsed) {
