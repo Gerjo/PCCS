6b101dc08358c993e6a9d11563cf45ae0008a334
sparkie <dark_sparkie@hotmail.com>
2012-10-29 16:43:33 +0100

    New settings

--- dedicatedserver/src/Settings.cpp
+++ dedicatedserver/src/Settings.cpp
@@ -5,9 +5,9 @@ double Settings::PING_GRACE_TIME   = 10.0;
 int Settings::LISTEN_PORT          = 8075;
 bool Settings::BROADCAST_SELF_ECHO = false;
 
-float Settings::BSP_WIDTH                    = 3000.0f;
-float Settings::BSP_HEIGHT                   = 3000.0f;
-float Settings::BSP_SMALLESTSIZE             = 30.0f;
+float Settings::BSP_WIDTH                    = 4000.0f;
+float Settings::BSP_HEIGHT                   = 4000.0f;
+float Settings::BSP_SMALLESTSIZE             = 20.0f;
 unsigned Settings::BSP_MAXCOLLISIONSPERSPACE = 50;
 
 void Settings::load(void) {
