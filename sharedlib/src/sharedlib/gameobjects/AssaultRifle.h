8e9576320c3d58bde207d938a802e6265b833c9c
Descalon <info@nicoglas.com>
2012-10-31 21:59:04 +0100

    Added Assault rifle

---
+++ sharedlib/src/sharedlib/gameobjects/AssaultRifle.h
@@ -0,0 +1,13 @@
+#ifndef ASSAULT_H
+#define ASSAULT_H
+
+#include "WeaponBehaviour.h"
+
+class LIBEXPORT AssaultRifle : WeaponBehaviour{
+
+    virtual void render(void);
+
+}
+
+
+#endif /* ASSAULT_H */
