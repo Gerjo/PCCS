78c917c0e496930674ab0a7fde7f24d41757d083
Gerjo <gerjoo@gmail.com>
2012-10-31 23:04:49 +0100

    Initial working commit that shows shooting over network. There is a huge delay though.

--- game/nbproject/Makefile-Release.mk
+++ game/nbproject/Makefile-Release.mk
@@ -35,8 +35,8 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/src/components/PreloaderText.o \
 	${OBJECTDIR}/src/Game.o \
+	${OBJECTDIR}/src/components/PreloaderText.o \
 	${OBJECTDIR}/src/InputField.o \
 	${OBJECTDIR}/src/components/KeyboardListener.o \
 	${OBJECTDIR}/src/gamestates/ClientWorld.o \
@@ -84,15 +84,15 @@ LDLIBSOPTIONS=-L../dist -Wl,-rpath,.
 	${MKDIR} -p ../dist
 	${LINK.cc} -Werror -lphantom -lglut -lyaxl -lsharedlib -o ../dist/guarrilla_tactics.run ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
-${OBJECTDIR}/src/components/PreloaderText.o: src/components/PreloaderText.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src/components
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -Werror -I/usr/local/include -I../phantom/src -I../phantom/include -I. -I../libyaxl/libyaxl -I../sharedlib/src -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/components/PreloaderText.o src/components/PreloaderText.cpp
-
 ${OBJECTDIR}/src/Game.o: src/Game.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Werror -I/usr/local/include -I../phantom/src -I../phantom/include -I. -I../libyaxl/libyaxl -I../sharedlib/src -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Game.o src/Game.cpp
+
+${OBJECTDIR}/src/components/PreloaderText.o: src/components/PreloaderText.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/components
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Werror -I/usr/local/include -I../phantom/src -I../phantom/include -I. -I../libyaxl/libyaxl -I../sharedlib/src -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/components/PreloaderText.o src/components/PreloaderText.cpp
 
 ${OBJECTDIR}/src/InputField.o: src/InputField.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
--- sharedlib/src/sharedlib/gameobjects/LightBullet.cpp
+++ sharedlib/src/sharedlib/gameobjects/LightBullet.cpp
@@ -4,7 +4,8 @@
 LightBullet::LightBullet() :
         _velocity(20, 20, 0),
         _direction(1, 1, 0),
-        _ttl(1) {
+        _ttl(1),
+        owner(0) {
 
     setType("Bullet");
     _boundingBox.size.x = 10;
@@ -20,8 +21,8 @@ LightBullet::~LightBullet() {
 void LightBullet::fromData(Data& data) {
     GameObject::fromData(data);
     _creationTime = data("creationtime");
-    _direction.x  = data("d-z");
-    _direction.y  = data("d-z");
+    _direction.x  = data("d-x");
+    _direction.y  = data("d-y");
     _direction.z  = data("d-z");
 
     // strategy = StrategyFactory::create(data("strategy"));
@@ -47,7 +48,7 @@ void LightBullet::update(const Time& time) {
     _position += _velocity * _direction;
 
     if(time.getTime() - _creationTime > _ttl) {
-        destroy();
+        //destroy();
     }
 }
 
@@ -62,7 +63,9 @@ void LightBullet::onCollision(Composite* entity) {
         return;
     }
 
-    owner->onKillSomething(static_cast<GameObject*>(entity));
+    if(owner != 0) {
+        owner->onKillSomething(static_cast<GameObject*>(entity));
+    }
 
     destroy();
     entity->destroy();
