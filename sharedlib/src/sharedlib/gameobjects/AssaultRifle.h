34ff9dd9e264ea534fbe0223f9432498b9fd4769
Descalon <info@nicoglas.com>
2012-11-01 13:48:06 +0100

    Added properties

--- sharedlib/src/sharedlib/gameobjects/AssaultRifle.h
+++ sharedlib/src/sharedlib/gameobjects/AssaultRifle.h
@@ -4,9 +4,17 @@
 #include "WeaponBehaviour.h"
 
 class LIBEXPORT AssaultRifle : public WeaponBehaviour{
+public:
+    AssaultRifle(): _rateOfFire(20), _range(20), _cooldownTimeSeconds(1), _lastShootTime(0){
+
+    }
 
     virtual void render(void);
-
+protected:
+    float _rateOfFire;
+    float _range;
+    double _cooldownTimeSeconds;
+    double _lastShootTime;
 };
 
 
