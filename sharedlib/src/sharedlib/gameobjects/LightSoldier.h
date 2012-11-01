fc6214b8e4b79f42bf4154f38e18f1cb18f6df85
Gerjo <gerjoo@gmail.com>
2012-11-01 10:50:45 +0100

    Merge error, forgot to remove this one.

--- sharedlib/src/sharedlib/gameobjects/LightSoldier.h
+++ sharedlib/src/sharedlib/gameobjects/LightSoldier.h
@@ -36,7 +36,6 @@ public:
 
 protected:
     virtual void onBulletFired(LightBullet* bullet);
-    virtual void init(void);
 
     vector<Vector3> _path; // For debugging only.
     GameObject* _victim;
