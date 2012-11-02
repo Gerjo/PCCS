c216521b033d6e76e614e22b4bb0fb266db4c66e
Descalon <info@nicoglas.com>
2012-11-02 14:58:06 +0100

    Crate now handles data

--- sharedlib/src/sharedlib/gameobjects/Crate.h
+++ sharedlib/src/sharedlib/gameobjects/Crate.h
@@ -1,16 +1,20 @@
 #ifndef CRATE_H
 #define CRATE_H
 
+#include <string>
+#include <algorithm>
 #include "GameObject.h"
+#include "behaviours/AbstractBehaviour.h"
 
 class LIBEXPORT Crate : public GameObject{
 public:
     Crate();
     void toData(Data& data);
     void fromData(Data& data);
-
+    void paint();
 private:
-    GameObject* _content;
+    AbstractBehaviour* createFromString(string typeName);
+    AbstractBehaviour* _content;
 };
 
 #endif /* CRATE_H */
