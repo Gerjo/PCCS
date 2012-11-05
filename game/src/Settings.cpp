3fdeeeb5f4f6b4347cca022f6b5223fb4bbdcbdd
Descalon <info@nicoglas.com>
2012-11-05 21:50:53 +0100

    Replaced the mission folder

--- game/src/Settings.cpp
+++ game/src/Settings.cpp
@@ -1,8 +1,8 @@
 #include "Settings.h"
 
-string Settings::SERVER_HOST   = "127.0.0.1";//"145.92.7.231";
+string Settings::SERVER_HOST   = "84.107.120.141";//"127.0.0.1";//"145.92.7.231";
 string Settings::SERVER_PORT   = "8075";
-string Settings::NICKNAME      = "sparkie";
+string Settings::NICKNAME      = "descalon";
 
 void Settings::load(void) {
     // load from file?
---
+++ sharedlib/src/sharedlib/missions/CapturePOI.h
@@ -0,0 +1,14 @@
+#ifndef CAPTURE_POINT_OF_INTEREST_H
+#define CAPTURE_POINT_OF_INTEREST_H
+
+#include "Objective.h"
+
+class LIBEXPORT CapturePOI : public Objective{
+public:
+    CapturePOI();
+    ~CapturePOI();
+
+
+}
+
+#endif /* CAPTURE_POINT_OF_INTEREST_H */
