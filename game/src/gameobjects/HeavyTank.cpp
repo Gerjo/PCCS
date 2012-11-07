1fedc2a64ca036ed3ba79797d8c0df69f045db87
Gerjo <gerjoo@gmail.com>
2012-11-07 12:11:08 +0100

    Fixed images.

--- game/src/gameobjects/HeavyTank.cpp
+++ game/src/gameobjects/HeavyTank.cpp
@@ -11,17 +11,59 @@ HeavyTank::~HeavyTank() {
 }
 
 void HeavyTank::paint() {
-    std::stringstream imageName;
-    imageName << "images/unit exports/shadows/Tank Bot 120x120 ";
-    ImageDirections::to8Directions(imageName, phantom::maths::directionToRotation(&_directionTur));
-    imageName << ".png";
+    std::stringstream tankBodyShadow;
+    tankBodyShadow << "images/unit exports/shadows/Tank Bot 120x120 ";
+    ImageDirections::to8Directions(tankBodyShadow, phantom::maths::directionToRotation(&_directionTur));
+    tankBodyShadow << " shadow.png";
 
-    std::stringstream imageName2;
-    imageName2 << "images/unit exports/shadows/Tank Tur 100x100 ";
-    ImageDirections::to8Directions(imageName2, phantom::maths::directionToRotation(&_direction));
-    imageName2 << ".png";
+    std::stringstream tankBody;
+    tankBody << "images/unit exports/shadows/Tank Bot 120x120 ";
+    ImageDirections::to8Directions(tankBody, phantom::maths::directionToRotation(&_directionTur));
+    tankBody << ".png";
 
-    getGraphics().clear().beginPath().setFillStyle(Colors::WHITE).image(imageName.str(), 0, 0, 120, 120).image(imageName2.str(), 10, 10, 100, 100).
-        rect(0, 0, _boundingBox.size.x * (_health / _totalHealth), 5).
-        fill().stroke();
+    std::stringstream tankTurretShadow;
+    tankTurretShadow << "images/unit exports/shadows/Tank Tur 100x100 ";
+    ImageDirections::to8Directions(tankTurretShadow, phantom::maths::directionToRotation(&_direction));
+    tankTurretShadow << " shadow.png";
+
+    std::stringstream tankTurret;
+    tankTurret << "images/unit exports/shadows/Tank Tur 100x100 ";
+    ImageDirections::to8Directions(tankTurret, phantom::maths::directionToRotation(&_direction));
+    tankTurret << ".png";
+
+    getGraphics()
+            .clear()
+            .beginPath()
+            .setFillStyle(Colors::WHITE)
+            .image(tankBodyShadow.str(), 0, 0, 120, 120)
+            .fill()
+            .stroke();
+
+    getGraphics()
+            .beginPath()
+            .setFillStyle(Colors::CORNFLOWER)
+            .image(tankBody.str(), 0, 0, 120, 120)
+            .fill()
+            .stroke();
+
+    getGraphics()
+            .beginPath()
+            .setFillStyle(Colors::WHITE)
+            .image(tankTurretShadow.str(), 10, 10, 100, 100)
+            .fill()
+            .stroke();
+
+    getGraphics()
+            .beginPath()
+            .setFillStyle(Colors::CORNFLOWER)
+            .image(tankTurret.str(), 10, 10, 100, 100)
+            .fill()
+            .stroke();
+
+    getGraphics()
+            .beginPath()
+            .setFillStyle(Colors::WHITE)
+            .rect(0, 0, _boundingBox.size.x * (_health / _totalHealth), 5)
+            .fill()
+            .stroke();
 }
