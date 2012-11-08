8607ba87c8cb92d0ee9ca49fcd2ca7916ac4afc9
sparkie <dark_sparkie@hotmail.com>
2012-11-08 15:46:14 +0100

    Scrolling with camera.

--- game/src/components/Console.cpp
+++ game/src/components/Console.cpp
@@ -33,7 +33,8 @@ Console::Console() : _doRedraw(true), _logCount(0), _enabled(false) {
 
 void Console::update(const Time& time) {
     Composite::update(time);
-
+    Vector3 *campos = &getDriver()->getActiveCameras()->at(0)->getPosition();
+    setPosition(*campos);
     getGraphics().clear();
 
     if(!_enabled && getDriver()->getInput()->getKeyboardState()->isKeyDown('\\')) {
