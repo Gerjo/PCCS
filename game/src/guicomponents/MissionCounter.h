ec9421e0a8b7a863f61aea8df9125823d9e8e2e3
Gerard Meier <gerjoo@gmail.com>
2012-11-11 16:11:27 +0100

    Apparently GCC does not let you create references to temp. variables.

--- game/src/guicomponents/MissionCounter.h
+++ game/src/guicomponents/MissionCounter.h
@@ -8,15 +8,19 @@ public:
     inline void drawMe(phantom::Graphics *g, phantom::Vector3 *cursorPosition) {
         phantom::Box3 _bounds(1550.0f, 60.0f, 370.0f, 48.0f);
 
+        string fontName = "fonts/arial.ttf";
+
         if(_bounds.contains(cursorPosition)) {
+            string text = "Blaat";
             g->beginPath().setFillStyle(phantom::Colors::MIDNIGHTBLUE);
-            //for(unsigned i = 0; i < 5; ++i)
-            //    g->text(_bounds.origin.x, _bounds.origin.y + (i * (_bounds.size.y)), static_cast<int>(_bounds.size.y / 2), "fonts/arial.ttf", std::string("Blaat"));
+            for(unsigned i = 0; i < 5; ++i)
+                g->text(_bounds.origin.x, _bounds.origin.y + (i * (_bounds.size.y)), static_cast<int>(_bounds.size.y / 2), fontName, text);
             g->stroke();
         }
         else {
+            string text = "Missions";
             g->beginPath().setFillStyle(phantom::Colors::WHITE);
-            //g->text(_bounds.origin.x, _bounds.origin.y, static_cast<int>(_bounds.size.y / 2), "fonts/arial.ttf", std::string("Missions: "));
+            g->text(_bounds.origin.x, _bounds.origin.y, static_cast<int>(_bounds.size.y / 2), fontName, text);
             g->stroke();
         }
     }
