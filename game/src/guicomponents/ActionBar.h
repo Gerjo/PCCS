52ee64653ade42904cb96f7ec829dd5575fbb1dc
sparkie <dark_sparkie@hotmail.com>
2012-11-16 11:53:27 +0100

    Actionbar looks like an actionbar again

--- game/src/guicomponents/ActionBar.h
+++ game/src/guicomponents/ActionBar.h
@@ -17,11 +17,11 @@ public:
             phantom::Vector3 mousepos = mousestate->getPosition();
 
             getGraphics().beginPath().setFillStyle(phantom::Colors::WHITE)
-                .image("images/hud menu/menu bg.png", loc[0], loc[1], 761.0f, 163.0f)
-                .image("images/hud menu/menu 1.png", loc[0] + 10.0f, loc[1] + 10.0f, 209.0f, 68.0f)
-                .image("images/hud menu/menu 2.png", loc[0] + 225.0f, loc[1] + 10.0f, 209.0f, 68.0f)
-                .image("images/hud menu/menu 3.png", loc[0] + 10.0f, loc[1] + 85.0f, 209.0f, 68.0f)
-                .image("images/hud menu/menu 4.png", loc[0] + 225.0f, loc[1] + 85.0f, 209.0f, 68.0f)
+                .image("images/hud menu/menu bg.png", loc[0], loc[1], 761.0f, 163.0f).stroke()
+                .image("images/hud menu/menu 1.png", loc[0] + 10.0f, loc[1] + 10.0f, 209.0f, 68.0f).stroke()
+                .image("images/hud menu/menu 2.png", loc[0] + 225.0f, loc[1] + 10.0f, 209.0f, 68.0f).stroke()
+                .image("images/hud menu/menu 3.png", loc[0] + 10.0f, loc[1] + 85.0f, 209.0f, 68.0f).stroke()
+                .image("images/hud menu/menu 4.png", loc[0] + 225.0f, loc[1] + 85.0f, 209.0f, 68.0f).stroke()
                 .image("images/hud menu/menu inventory.png", loc[0] + 450.0f, loc[1] + 10.0f, 296.0f, 144.0f)
                 .stroke();
 
