d90376391b0e4aa8207d38ccab6ad9fc2a3d1779
sparkie <dark_sparkie@hotmail.com>
2012-11-16 10:41:28 +0100

    Allowed to have a default server if a nickname is selected.

--- game/src/Settings.cpp
+++ game/src/Settings.cpp
@@ -20,8 +20,8 @@ void Settings::load(void) {
     try {
         Data data = Data::fromJson(yaxl::file::File("conf/settings.json").readAll());
 
-        SERVER_HOST = data("serverhost").toString();
-        SERVER_PORT = data("serverport").toString();
+        if(data("serverhost").toString() != "") SERVER_HOST = data("serverhost").toString();
+        if(data("serverport").toString() != "") SERVER_PORT = data("serverport").toString();
         NICKNAME    = data("nickname").toString();
     } catch (yaxl::file::FileException& e) {
         std::cout << e.what() << "Proceeding with default settings." << endl;
