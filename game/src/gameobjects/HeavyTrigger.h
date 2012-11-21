0c33420b7f1602e976c547993c153d2f5f73873a
sparkie <dark_sparkie@hotmail.com>
2012-11-21 14:36:49 +0100

    Added an HeavyTrigger (on debug mode we can see where the Trigger is added).

---
+++ game/src/gameobjects/HeavyTrigger.h
@@ -0,0 +1,13 @@
+#ifndef HEAVYTRIGGER_H_
+#define HEAVYTRIGGER_H_
+
+#include <sharedlib/gameobjects/LightTrigger.h>
+
+class HeavyTrigger : public LightTrigger {
+public:
+    HeavyTrigger();
+
+    void paint();
+};
+
+#endif
