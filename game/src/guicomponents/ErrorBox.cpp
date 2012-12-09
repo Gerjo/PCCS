85bd0a4989e4692ffe4e01b2dddfd7078fd0893c
sparkie <dark_sparkie@hotmail.com>
2012-12-09 16:40:06 +0100

    The start of a beautiful error box. (UNFINISHED.)

---
+++ game/src/guicomponents/ErrorBox.cpp
@@ -0,0 +1,37 @@
+#include "ErrorBox.h"
+
+ErrorBox::ErrorBox(string message) : _repaint(true) {
+    const Vector3 &vp = getPhantomGame()->getViewPort();
+    Box3 bb = Box3(0, 0, 725, 396);
+    this->setBoundingBox(bb);
+    this->setPosition(Vector3((vp.x - bb.size.x) / 2, (vp.y - bb.size.y) / 2, 0.0f));
+    this->_message = message;
+    this->_okButton = new MenuButton();
+    this->_okButton->setBoundingBox(Box3(0, 0, 525, 100));
+    this->_okButton->setPosition(Vector3(100, 250, 0));
+    this->_okButton->setText("Okay");
+    addComponent(this->_okButton);
+}
+
+ErrorBox::~ErrorBox() {
+    delete this->_okButton;
+}
+
+void ErrorBox::paint() {
+    const Box3 &bb = this->getBoundingBox();
+    unsigned fsize = 20;
+
+    getGraphics().clear().beginPath().setFillStyle(Colors::WHITE).
+        image("images/menu/Error.png", 0, 0, bb.size.x, bb.size.y).fill().
+        text(10, 10, fsize, "fonts/waree.ttf", this->_message).fill().
+        text(10, 60, fsize, "fonts/waree.ttf", "A bug report has been sent.").fill();
+
+    this->_okButton->paint();
+
+    _repaint = false;
+}
+
+void ErrorBox::update(const phantom::PhantomTime &time) {
+    if(_repaint)
+        paint();
+}
