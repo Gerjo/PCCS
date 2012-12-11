0c42e3866f1ca2f107779724975cf70ba37d549c
Gerard Meier <gerjoo@gmail.com>
2012-12-11 22:28:39 +0100

    Fixed bug. The source location should've been the player (the flocker), not the leader. Do'h.

--- sharedlib/src/sharedlib/artificialintelligence/squad/SquadFlocking.cpp
+++ sharedlib/src/sharedlib/artificialintelligence/squad/SquadFlocking.cpp
@@ -20,17 +20,16 @@ void SquadFlocking::construct() {
 void SquadFlocking::handle(const phantom::PhantomTime& time) {
     if(_updateInterval.hasExpired(time)) {
         _updateInterval.restart();
-
-
-        BSPTree* tree            = static_cast<BSPTree*>(_leader->getLayer());
+        Entity* gameobject       = ai->getOwner<Entity*>();
+        BSPTree* tree            = static_cast<BSPTree*>(gameobject->getLayer());
         Pathfinding* pathfinding = tree->pathfinding;
-        RouteDetails route = pathfinding->getPathDetailled(_leader, _leader->getBoundingBox().getCenter());
+        RouteDetails route = pathfinding->getPathDetailled(gameobject, _leader->getBoundingBox().getCenter());
 
 
-        //if(route.distanceSq > 1.0f) {
+        if(route.distanceSq > 1.0f) {
             Message<Pathfinding::Route> message("mover-set-path", route.route);
             ai->getParent()->handleMessage(&message);
-        //}
+        }
 
     }
 }
