907c01d00c8f5bdd9439877c7378fceec3f66a26
Gerard Meier <gerjoo@gmail.com>
2012-12-16 17:28:28 +0100

    to_string uses too many trailing zeros.

--- sharedlib/src/sharedlib/serialization/Data.cpp
+++ sharedlib/src/sharedlib/serialization/Data.cpp
@@ -32,7 +32,10 @@ Data& Data::operator=(const int& value) {
 
 Data& Data::operator=(const float& value) {
     _isSubset = false;
-    _raw      = std::to_string(value);
+
+    stringstream ss;
+    ss << value;
+    _raw = std::move(ss.str());
 
     return *this;
 }
